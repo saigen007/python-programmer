ceaddc16945dd1a930735db8956358d861b44fa8
saigen007 <36730754+saigen007@users.noreply.github.com>
2018-02-23 14:47:48 +0530

    Create vowel or consonant.py

---
+++ beginner level/vowel or consonant.py
@@ -0,0 +1,8 @@
+a=raw_input("enter the letter")
+list = ['a','e','i','o','u','A','E','I','O','u']
+if a in list:
+    print("it is vowel")
+elif("a<0"and"a>9"):
+    print("it is number")
+else:
+    print("it is consonant")
